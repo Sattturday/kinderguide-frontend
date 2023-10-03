df30b7f6b3f9523cb496ed1c3c5c348627a43e64
Max Teplyakov <maxxtepll@gmail.comgit config --listgit config --global user.email maxxtepll@gmail.com>
2023-10-03 22:51:28 +0400

    Feat/card
    Создал компонент Card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,28 @@
+import './Card.scss';
+
+export const Card = () => (
+  <div className='card'>
+    <img className='card__img' src='#' alt='Фото школы' />
+    <div className='card__container'>
+      <div className='card__title-block'>
+        <h2 className='card__title'>Название школы</h2>
+        <div className='card__rating-block'>
+          <div className='card__star' />
+          <p className='card__counting'>(1)</p>
+        </div>
+      </div>
+      <p className='card__text'>
+        Краткая информация о школе Краткая информация о школеКраткая информация
+        о школеКраткая информация о школеКраткая информация о школе
+      </p>
+      <div className='card__price-block'>
+        <p className='card__price'>
+          от <span className='card__number'>1000</span> &#8381;/мес
+        </p>
+        <div className='card__like-block'>
+          <button className='card__like-btn' />
+        </div>
+      </div>
+    </div>
+  </div>
+);
