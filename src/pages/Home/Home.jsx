9ad9a0af6691f41612bbde5383b8fae3e42af1bc
YuliaMenachoPages <yumenachopages@gmail.com>
2023-10-06 12:34:53 +0300

    feat(form): created Form pattern

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import './Home.scss';
 import { InputCheckbox } from '../../components/InputCheckbox';
-import { Sort } from '../../components/common/Sort/Sort';
-import { NotFoundPage } from '../NotFoundPage/NotFoundPage';
+import { Sort } from '../../components/common/Sort';
+import { NotFoundPage } from '../NotFoundPage';
 import { News } from './components/News';
+import Form from '../../components/Form/Form';
 
 export function Home() {
   return (
@@ -11,6 +13,19 @@ export function Home() {
       <News />
       <InputCheckbox />
       <Sort />
+      <Form title='Регистрация' variant='login' button='Register' />
+      <Form
+        title='Восстановление пароля ..................................................'
+        variant='recovery'
+        subtitle='test'
+        button='Recover'
+      >
+        <p className='test'>
+          Не получили email? Проверьте спам или{' '}
+          <a href='www.example.com'>попробуйте снова</a>
+        </p>
+        <img src='https://via.placeholder.com/217x197/' alt='people' />
+      </Form>
       <NotFoundPage />
     </div>
   );
