bf54fdbcc1eaa52c046db7c443755d4155854fec
MickKrishtopa <m.a.krishtopa@yandex.ru>
2023-10-07 15:27:32 +0300

    feat(promo): make component Promo and add to the Home page

--- src/pages/Home/components/Promo/Promo.jsx
+++ src/pages/Home/components/Promo/Promo.jsx
@@ -1,16 +1,42 @@
 import './Promo.scss';
 import { Button } from '../../../../components/common/Button';
+import Union1 from '../../../../images/Promo/Union1.svg';
+import Union2 from '../../../../images/Promo/Union2.svg';
+import Union3 from '../../../../images/Promo/Union3.svg';
+import arrow from '../../../../images/Promo/Arrow.svg';
 
 export function Promo() {
   return (
     <section className='promo'>
-      <div className='wrapper'>
-        <div className='promo__description-area'>
-          <h2>
-            Помогаем найти лучший образовательный центр для вашего ребёнка.
-          </h2>
-          <Button />
-        </div>
+      <div className='promo__description-area'>
+        <h2 className='promo__title'>
+          Помогаем найти лучший образовательный центр для вашего ребёнка.
+        </h2>
+        <Button width='408px' size='large'>
+          Перейти в каталог
+        </Button>
+      </div>
+      <div className='promo__image-container'>
+        <img
+          className='promo__image promo__image_top-left'
+          src={Union1}
+          alt='фотография ребенка'
+        />
+        <img
+          className='promo__image promo__image_top-right'
+          src={Union2}
+          alt='показывают большой палец вверх'
+        />
+        <img
+          className='promo__image promo__image_bottom'
+          src={Union3}
+          alt='дети смотрят глобус'
+        />
+        <img
+          className='promo__image promo__image_arrow'
+          src={arrow}
+          alt='пунктирная стрелка'
+        />
       </div>
     </section>
   );
