ac62d8552978f8c391503858258b6aaf19195f8a
Alla Magina <babara@flylady.su>
2023-10-09 11:55:43 +0300

    feat(popup): add Popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Layout from './layouts/main';
@@ -10,8 +11,29 @@ import { Specialists } from './pages/Specialists';
 import { NotFoundPage } from './pages/NotFoundPage';
 import { Parents } from './pages/Parents';
 import { Profile } from './pages/Profile';
+import { Popup } from './components/common/Popup';
+import { ImagePopup } from './components/common/ImagePopup';
 
 function App() {
+  // Стейты для попапов
+  const [infoMessage, setInfoMessage] = useState('');
+  const [image, setImage] = useState('');
+
+  // Для просмотра работы попапа с непустыми значениями,
+  // раскомментируйте одну из следующих пар строк и закомментируйте пустые значения
+
+  // const [infoMessage, setInfoMessage] = useState(
+  //   'Помогаем найти лучший образовательный центр для вашего ребёнка.'
+  // );
+  // const [image, setImage] = useState(
+  //   'https://images.unsplash.com/photo-1696655496030-079414c31e06?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80'
+  // );
+
+  function closeAllPopups() {
+    setInfoMessage('');
+    setImage('');
+  }
+
   return (
     <Layout>
       <Routes>
@@ -25,6 +47,10 @@ function App() {
         <Route path='/recovery' element={<Recovery />} />
         <Route path='*' element={<NotFoundPage />} />
       </Routes>
+      <Popup isOpen={infoMessage} name='info' onClose={closeAllPopups}>
+        <p className='popup__title popup__title_info'>{infoMessage}</p>
+      </Popup>
+      <ImagePopup image={image} onClose={closeAllPopups} />
     </Layout>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './Home.scss';
 import { Promo } from './components/Promo';
-import { Resources } from './components/Recurses';
+import { Resources } from './components/Resources';
 import { About } from './components/About';
 
 export function Home() {
