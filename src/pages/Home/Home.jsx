175dfc3a33c9c8cb3e1e448b5122a13bde12b4b8
YuliaMenachoPages <yumenachopages@gmail.com>
2023-10-09 18:59:31 +0300

    fix(home): deleted import from Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,13 +3,11 @@ import React from 'react';
 import './Home.scss';
 import { Promo } from './components/Promo';
 import { Resources } from './components/Recurses';
-import { InputPassword } from '../../components/InputPassword';
 
 export function Home() {
   return (
     <>
       <Promo />
-      <InputPassword variant='password' />
       <Resources />
     </>
   );
