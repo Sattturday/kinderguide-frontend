ac5df28db62c7068f615029c28a323c68a92c09e
Max Teplyakov <maxxtepll@gmail.comgit config --listgit config --global user.email maxxtepll@gmail.com>
2023-10-11 12:16:15 +0400

    feat(profile): сверстал страницу Profile
    сверстал страницу Profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Profile.scss';
+import { UserEdit } from './components/UserEdit/UserEdit';
+import { User } from './components/User/User';
+import { NavigationProfile } from './components/NavigationProfile/NavigationProfile';
 
 export function Profile() {
-  return <div>Profile</div>;
+  const [editUser, setEditUser] = useState(false);
+  return (
+    <section className='profile'>
+      <div className='wrapper'>
+        <h1 className='profile__title'>Личный кабинет</h1>
+        <div className='profile__block'>
+          <NavigationProfile />
+          <section className='user'>
+            <div className='user__avatar' />
+            {editUser ? (
+              <UserEdit setEditUser={setEditUser} />
+            ) : (
+              <User setEditUser={setEditUser} />
+            )}
+          </section>
+        </div>
+      </div>
+    </section>
+  );
 }
