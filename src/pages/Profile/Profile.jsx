1361df30c0d33dc2d2c103b60123064c643ce026
Max Teplyakov <maxxtepll@gmail.comgit config --listgit config --global user.email maxxtepll@gmail.com>
2023-10-12 15:01:56 +0400

    feat(profile): добавил хлебные крошки

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -3,12 +3,14 @@ import './Profile.scss';
 import { UserEdit } from './components/UserEdit/UserEdit';
 import { User } from './components/User/User';
 import { NavigationProfile } from './components/NavigationProfile/NavigationProfile';
+import { BreadCrumb } from './components/BreadCrumb/BreadCrumb';
 
 export function Profile() {
   const [editUser, setEditUser] = useState(false);
   return (
     <section className='profile'>
       <div className='wrapper'>
+        <BreadCrumb />
         <h1 className='profile__title'>Личный кабинет</h1>
         <div className='profile__block'>
           <NavigationProfile />
