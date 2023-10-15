c18972a75a2012e6a60017f927bae281ea282269
YuliaMenachoPages <yumenachopages@gmail.com>
2023-10-15 19:22:35 +0300

    feat(orgwrapper, school, kindergarten): added page of organization

--- src/components/OrgWrapper/Feedback/Feedback.jsx
+++ src/components/OrgWrapper/Feedback/Feedback.jsx
@@ -3,20 +3,37 @@ import { Stars } from '../../Stars';
 import { Button } from '../../common/Button';
 
 export const Feedback = ({ feedback }) => {
-  return feedback?.map((fb) => (
-    <div className='feedback'>
-      <div className='feedback__titleWrapper'>
-        <h3 className='feedback__title'>{fb.author}Test</h3>
-        <Stars rating={fb.grade} />
+  return (
+    <div>
+      {feedback?.map((fb) => (
+        <div className='feedback'>
+          <div className='feedback__titleWrapper'>
+            <h3 className='feedback__title'>{fb.author}Test</h3>
+            <Stars rating={fb.grade} />
+          </div>
+          <p className='feedback__text'>
+            {fb.content}
+            Мы перевели нашего сына Марка в этом году. Ранее мы учились в другой
+            школе, но нам там не понравилось ни мне, ни сыну из-за отсутствия
+            индивидуального подхода к детям test test test test test test test
+            test test
+          </p>
+          <Button variant='link'>Подробнее></Button>
+        </div>
+      ))}
+      <div className='feedback'>
+        <div className='feedback__titleWrapper'>
+          <h3 className='feedback__title'>Test</h3>
+          <Stars rating='5' />
+        </div>
+        <p className='feedback__text'>
+          Мы перевели нашего сына Марка в этом году. Ранее мы учились в другой
+          школе, но нам там не понравилось ни мне, ни сыну из-за отсутствия
+          индивидуального подхода к детям test test test test test test test
+          test test
+        </p>
+        <Button variant='link'>Подробнее></Button>
       </div>
-      <p className='feedback__text'>
-        {fb.content}
-        Мы перевели нашего сына Марка в этом году. Ранее мы учились в другой
-        школе, но нам там не понравилось ни мне, ни сыну из-за отсутствия
-        индивидуального подхода к детям test test test test test test test test
-        test
-      </p>
-      <Button variant='link'>Подробнее></Button>
     </div>
-  ));
+  );
 };
--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,28 @@ import { configureStore } from '@reduxjs/toolkit';
 import modalsReducer from './modalsSlice';
 import { authApi } from '../api/authApi';
 import { userApi } from '../api/userApi';
+import { schoolApi } from '../api/schoolApi';
+import { kindergartenApi } from '../api/kindergartenApi';
+import { schoolReviewsApi } from '../api/schoolReviewsApi';
+import { kindergartenReviewsApi } from '../api/kindergartenReviewsApi';
 
 export default configureStore({
   reducer: {
     modals: modalsReducer,
     [authApi.reducerPath]: authApi.reducer,
-    // [schoolApi.reducerPath]: schoolApi.reducer,
+    [schoolApi.reducerPath]: schoolApi.reducer,
+    [kindergartenApi.reducerPath]: kindergartenApi.reducer,
+    [schoolReviewsApi.reducerPath]: schoolReviewsApi.reducer,
+    [kindergartenReviewsApi.reducerPath]: kindergartenReviewsApi.reducer,
   },
 
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware, userApi.middleware),
+    getDefaultMiddleware().concat(
+      authApi.middleware,
+      userApi.middleware,
+      schoolApi.middleware,
+      kindergartenApi.middleware,
+      schoolReviewsApi.middleware,
+      kindergartenReviewsApi.middleware
+    ),
 });
