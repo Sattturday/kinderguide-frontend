a103c5bcc3c9eb687f84c42e0aa3ab2c2c7ca27e
Alla Magina <babara@flylady.su>
2023-10-17 21:32:08 +0300

    chore: correct Slider & ImageSlider

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,10 +3,15 @@ import { Promo } from './components/Promo';
 import { Resources } from './components/Resources';
 import { About } from './components/About';
 import { ServiceWork } from './components/ServiceWork';
+import { ImageSlider } from '../../components/ImageSlider';
+import { album, exampleSlides } from '../../utils/constants';
+import { Slider } from '../../components/common/Slider/Slider';
 
 export function Home() {
   return (
     <>
+      <Slider slides={exampleSlides} />
+      <ImageSlider images={album} width='738px' />
       <Promo />
       <About />
       <ServiceWork />
