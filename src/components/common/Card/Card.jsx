eff3e3afe42a6834dc56f05293d0d30aa159f8da
Alla Magina <babara@flylady.su>
2023-10-18 18:27:40 +0300

    chore(card&showlist): correct styles for Card & ShowList

--- src/components/common/Card/Card.jsx
+++ src/components/common/Card/Card.jsx
@@ -1,8 +1,21 @@
 import { useState } from 'react';
+
+import img from '../../../images/NewsCard/img1.jpg';
+import { Stars } from '../../Stars';
 import './Card.scss';
 
 export const Card = () => {
   const [isLiked, setIsLiked] = useState(false);
+
+  const cardData = {
+    id: 0,
+    name: 'Название школы',
+    description:
+      'Краткая информация о школе Краткая информация о школеКраткая информация о школеКраткая информация о школеКраткая информация о школе',
+    image: img,
+    price: 1000,
+    rating: 3.7,
+  };
 
   const cardLikeToggleClass = isLiked ? 'card__like-btn_active' : '';
   // Добавление лайка
@@ -12,27 +25,10 @@ export const Card = () => {
 
   return (
     <div className='card'>
-      <img
-        className='card__img'
-        src='https://via.placeholder.com/217x197/'
-        alt='Фото школы'
-      />
+      <img className='card__img' src={cardData.image} alt='Фото школы' />
       <div className='card__container'>
         <div className='card__title-block'>
-          <h2 className='card__title'>Название школы</h2>
-          <div className='card__rating-area'>
-            <div className='card__star' />
-            <p className='card__counting'>(1)</p>
-          </div>
-        </div>
-        <p className='card__text'>
-          Краткая информация о школе Краткая информация о школеКраткая
-          информация о школеКраткая информация о школеКраткая информация о школе
-        </p>
-        <div className='card__price-block'>
-          <p className='card__price'>
-            от <span className='card__number'>1000</span> &#8381;/мес
-          </p>
+          <h3 className='card__title'>{cardData.name}</h3>
           <div className='card__like-block'>
             <button
               className={`card__like-btn ${cardLikeToggleClass}`}
@@ -40,6 +36,13 @@ export const Card = () => {
               onClick={handleLike}
             />
           </div>
+        </div>
+
+        <p className='card__text'>{cardData.description}</p>
+
+        <div className='card__price-block'>
+          <p className='card__price'>{`от ${cardData.price} ₽/мес.`}</p>
+          <Stars rating={cardData.rating} />
         </div>
       </div>
     </div>
