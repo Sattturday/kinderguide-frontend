6b876b1e2423025d8e0ac383bd35de7c26ba81cb
Alla Magina <babara@flylady.su>
2023-10-18 19:54:38 +0300

    feat(likebutton): add LikeButton

--- src/components/common/Card/Card.jsx
+++ src/components/common/Card/Card.jsx
@@ -3,9 +3,14 @@ import { useState } from 'react';
 import img from '../../../images/NewsCard/img1.jpg';
 import { Stars } from '../../Stars';
 import './Card.scss';
+import { LikeButton } from '../../LikeButton/LikeButton';
 
 export const Card = () => {
   const [isLiked, setIsLiked] = useState(false);
+
+  const handleLike = () => {
+    setIsLiked(!isLiked);
+  };
 
   const cardData = {
     id: 0,
@@ -17,25 +22,13 @@ export const Card = () => {
     rating: 3.7,
   };
 
-  const cardLikeToggleClass = isLiked ? 'card__like-btn_active' : '';
-  // Добавление лайка
-  const handleLike = () => {
-    setIsLiked(!isLiked);
-  };
-
   return (
     <div className='card'>
       <img className='card__img' src={cardData.image} alt='Фото школы' />
       <div className='card__container'>
         <div className='card__title-block'>
           <h3 className='card__title'>{cardData.name}</h3>
-          <div className='card__like-block'>
-            <button
-              className={`card__like-btn ${cardLikeToggleClass}`}
-              aria-label='Поставить лайк'
-              onClick={handleLike}
-            />
-          </div>
+          <LikeButton isLiked={isLiked} onLike={handleLike} />
         </div>
 
         <p className='card__text'>{cardData.description}</p>
