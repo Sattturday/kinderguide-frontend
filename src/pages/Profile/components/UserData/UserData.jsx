c83f01f6d50c168cc6329c3e09ba52e8ce7b9332
Max Teplyakov <maxxtepll@gmail.comgit config --listgit config --global user.email maxxtepll@gmail.com>
2023-10-20 15:20:00 +0400

    fix(profile): изменил отображение данных пользователя

--- src/pages/Profile/components/UserData/UserData.jsx
+++ src/pages/Profile/components/UserData/UserData.jsx
@@ -4,27 +4,29 @@ import { Button } from '../../../../components/common/Button';
 import { useGetUserQuery } from '../../../../api/userApi';
 
 export function UserData({ setEditUser }) {
-  const { data = {} } = useGetUserQuery();
+  const { data = {}, isLoading } = useGetUserQuery();
 
   return (
     <>
       <div className='user__wrapper user__wrapper_top'>
         <p className='user__label'>Имя родителя</p>
-        <p className='user__text'>{`${data.first_name} ${data.last_name}`}</p>
+        <p className='user__text'>
+          {isLoading ? ' ' : `${data.first_name} ${data.last_name}`}
+        </p>
       </div>
       <div className='user__wrapper'>
         <p className='user__label'>Имя ребёнка</p>
         <p className='user__text'>
-          {`${data.child_first_name} ${data.child_last_name}`}
+          {isLoading ? ' ' : `${data.child_first_name} ${data.child_last_name}`}
         </p>
       </div>
       <div className='user__wrapper'>
         <p className='user__label'>Телефон</p>
-        <p className='user__text'>{`${data.phone}`}</p>
+        <p className='user__text'>{isLoading ? ' ' : `${data.phone}`}</p>
       </div>
       <div className='user__wrapper user__wrapper_bottom'>
         <p className='user__label'>Email</p>
-        <p className='user__text'>{`${data.email}`}</p>
+        <p className='user__text'> {isLoading ? ' ' : `${data.email}`}</p>
       </div>
       <Button
         type='button'
