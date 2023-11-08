007e25b5c4575877f3fa6f8fce3991a2753df85e
MickKrishtopa <m.a.krishtopa@yandex.ru>
2023-11-08 20:57:23 +0300

    feat(school and kindergarten): add new review
    added the ability to add new reviews for schools and kindergartens

--- src/api/kindergartenReviewsApi.js
+++ src/api/kindergartenReviewsApi.js
@@ -4,12 +4,41 @@ import { BASE_URL } from '../utils/constants';
 
 export const kindergartenReviewsApi = createApi({
   reducerPath: 'kindergartenReviewsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  tagTypes: ['KindergartenReviews'],
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+    prepareHeaders: (headers, { getState }) => {
+      const token = localStorage.getItem('token');
+
+      if (token) {
+        headers.set('authorization', `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
   endpoints: (build) => ({
     getKindergartenReviews: build.query({
       query: (kindergarten_id) => `kindergartens/${kindergarten_id}/reviews/`,
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: 'KindergartenReviews', id })),
+              { type: 'KindergartenReviews', id: 'LIST' },
+            ]
+          : [{ type: 'KindergartenReviews', id: 'LIST' }],
+    }),
+    createKindergartenReview: build.mutation({
+      query: ({ kindergarten_id, body }) => ({
+        url: `kindergartens/${kindergarten_id}/reviews/`,
+        method: 'POST',
+        body,
+      }),
+      invalidatesTags: [{ type: 'KindergartenReviews', id: 'LIST' }],
     }),
   }),
 });
 
-export const { useGetKindergartenReviewsQuery } = kindergartenReviewsApi;
+export const {
+  useGetKindergartenReviewsQuery,
+  useCreateKindergartenReviewMutation,
+} = kindergartenReviewsApi;
