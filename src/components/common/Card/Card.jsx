226b1af5636058e5b60dfb984f740380fa79d82a
YuliaMenachoPages <yumenachopages@gmail.com>
2023-11-11 00:25:28 +0300

    fix(card, likebutton): changed click from header to card

--- src/components/common/Card/Card.jsx
+++ src/components/common/Card/Card.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { Stars } from '../../Stars';
 import { LikeButton } from '../../LikeButton/LikeButton';
@@ -9,18 +9,22 @@ import img from '../../../images/NewsCard/img5.png';
 export const Card = ({ cardData, selected }) => {
   const [isLiked, setIsLiked] = useState(false);
   const image = cardData.album.length > 0 ? cardData.album[0].image : img;
+  const navigate = useNavigate();
+
   const handleLike = () => {
     setIsLiked(!isLiked);
   };
 
+  const handleClick = () => {
+    navigate(`/${selected}/${cardData.id}`);
+  };
+
   return (
-    <div className='card'>
+    <div className='card' onClick={handleClick}>
       <img className='card__img' src={image} alt='Фото школы или сада' />
       <div className='card__container'>
         <div className='card__title-block'>
-          <Link to={`/${selected}/${cardData.id}`} className='card__title'>
-            {cardData.name}
-          </Link>
+          <h3 className='card__title'>{cardData.name}</h3>
           <LikeButton isLiked={isLiked} onLike={handleLike} />
         </div>
 
