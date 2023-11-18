2f5deb2423fec76c8150f520d833217eaf8faa96
MickKrishtopa <m.a.krishtopa@yandex.ru>
2023-11-18 19:53:41 +0300

    fix authID

--- src/pages/Home/components/Promo/Promo.jsx
+++ src/pages/Home/components/Promo/Promo.jsx
@@ -1,12 +1,6 @@
 import { Link } from 'react-router-dom';
 
 import { Button } from '../../../../components/common/Button';
-import star from './images/star.svg';
-import glass from './images/glass.svg';
-import question from './images/question.svg';
-import dots from './images/dots.svg';
-import snake from './images/snake.svg';
-import girl from './images/girl.svg';
 
 import './Promo.scss';
 
@@ -35,16 +29,6 @@ export function Promo() {
             </Button>
           </div>
         </div>
-        {/* <div className='promo__image-container'>
-          <img src={star} alt='Какой детский сад лучше?' />
-          <img src={glass} alt='Как найти хорошую школу?' />
-          <img src={question} alt='Вопросительный знак' />
-          <img src={snake} alt='Декоративная линия' />
-          <img src={dots} alt='Декоративные точки' />
-          <img src={girl} alt='Девушка' />
-          <span>Как найти хорошую школу?</span>
-          <span>Какой детский сад лучше?</span>
-        </div> */}
       </div>
     </section>
   );
