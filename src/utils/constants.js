e77adf012d48b39c39063a5bedb6a3d7e69fd4a5
Alla Magina <babara@flylady.su>
2023-12-08 20:27:12 +0300

    fix: correct BASE_URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://kinder.acceleratorpracticum.ru/api/v1/';
+export const BASE_URL = 'https://mykinderguide.ru/api/v1/';
 
 export const EMAIL_PATTERN = '[a-zA-Z0-9_.]+@[a-zA-Z0-9_]+\\.[a-z]{2,}';
